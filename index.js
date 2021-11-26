944ade1bdce7f3e679dae35c1c201e9a28f4f4c9
Sagar <sagarpaul2k16@gmail.com>
2021-11-26 12:10:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const app = express();
+
+app.use(cors());
+app.use("/static", express.static("./static"));
+const port = process.env.PORT || 5000;
+
+app.get("/get", (req, res) => {
+  const url = req.query.url;
+  console.log(url);
+
+  (async () => {
+    const info = await ytdl.getInfo(url);
+    const title = info.videoDetails.title;
+    const thumbnail = info.videoDetails.thumbnails[0].url;
+    const formats = info.formats;
+
+    const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    // const format = ytdl.chooseFormat(info.formats, { quality: "136" });
+
+    res.send({ title, thumbnail, audioFormats, formats });
+  })();
+});
+
+app.get("/download", async (req, res) => {
+  const url = req.query.url;
+  const itag = req.query.itag;
+  const type = req.query.type;
+
+  const info = await ytdl.getInfo(url);
+  const title = info.videoDetails.title;
+
+  res.header("Content-Disposition", `attachment;  filename="${title}.${type}"`);
+  ytdl(url, { itag }).pipe(res);
+});
+
+app.listen(port, () => {
+  console.log("hey !!!");
+});
