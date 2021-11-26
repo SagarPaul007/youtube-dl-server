50ed95109d2b6706398eda56f39ea256b1557e70
Sagar <sagarpaul2k16@gmail.com>
2021-11-26 18:29:56 +0530

    changes in getting info

--- index.js
+++ index.js
@@ -8,25 +8,22 @@ app.use("/static", express.static("./static"));
 const port = process.env.PORT || 5000;
 
 app.get("/", (res) => {
-  res.sendFile(`index.html`, { root: "./" });
+  res.sendFile("./index.html");
 });
 
-app.get("/get", (req, res) => {
+app.get("/get", async (req, res) => {
   const url = req.query.url;
   console.log(url);
+  const info = await ytdl.getInfo(url);
+  const title = info.videoDetails.title;
+  const thumbnail = info.videoDetails.thumbnails[0].url;
+  let formats = info.formats;
 
-  (async () => {
-    const info = await ytdl.getInfo(url);
-    const title = info.videoDetails.title;
-    const thumbnail = info.videoDetails.thumbnails[0].url;
-    let formats = info.formats;
+  const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+  // const format = ytdl.chooseFormat(info.formats, { quality: "136" });
+  formats = formats.filter((format) => format.hasAudio === true);
 
-    const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-    // const format = ytdl.chooseFormat(info.formats, { quality: "136" });
-    formats = formats.filter((format) => format.hasAudio === true);
-
-    res.send({ title, thumbnail, audioFormats, formats });
-  })();
+  res.send({ title, thumbnail, audioFormats, formats });
 });
 
 app.get("/download", async (req, res) => {
